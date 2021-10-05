563491c73a9d3403535cb23db73d99290de45099
UssrFantom <dimkakrohin@mail.ru>
2021-10-06 00:28:21 +0300

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,43 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp();
+  runApp(MyFirstApp());
 }
 
+class MyFirstApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.indigo,
+        appBar: AppBar(
+          title: const Text('My First App'),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Container(
+            padding: EdgeInsets.all(16),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const <Widget>[
+                LinearProgressIndicator(
+                  value: 23,
+                ),
+                Text(
+                  '23%',
+                  style: TextStyle(color: Colors.white, fontSize: 20),
+                ),
+                Text(
+                  'Press button to download',
+                  style: TextStyle(color: Colors.white, fontSize: 20),
+                ),
+              ],
+            ),
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(onPressed: null,
+        child: Icon(Icons.cloud_download),),
+      ),
+    );
+  }
+}
